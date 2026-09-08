2539c4e6f3abc83b7d57247bce428b262eccd372
Valentina Liu <113727285+vliuliuu@users.noreply.github.com>
2026-09-07 22:45:46 -0700

    Add: grumpy-bookstore-owner - submission-0

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        //minute window
+        //brute force: for every interval we evaluate the final result of satisfaction to max satisfaction
+        // i for loop, as long as i < customers.length() - minutes
+        int end = customers.size() - (minutes - 1);
+
+        int maxSat = 0;
+        int regSat = 0;
+
+        for(int i = 0; i < customers.size(); i++){
+            //calculating the regular lvl of satisfaction
+            if(grumpy[i] == 0){
+                regSat += customers[i];
+            }
+        }
+
+        for(int i = 0; i < end; i++){
+
+            int techniqueSat = 0;
+
+            for(int j = i; j < (i + minutes); j++){
+
+                //technique calc, only adding where the dude is grumpy
+                if(grumpy[j] == 1){
+                    techniqueSat += customers[j];
+                }
+
+                if(techniqueSat > maxSat){
+                    maxSat = techniqueSat;
+                }
+
+            }
+
+        }
+        //add it together
+        maxSat += regSat;
+        return maxSat;
+    }
+};
